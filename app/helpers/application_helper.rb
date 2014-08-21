b0b675fd6014f06c9a30ceeb99596fa0ca853d67
Kevin Kang <kevinkang88@gmail.com>
2014-08-20 22:27:35 -0700

    created to_price method in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     promo = Promotion.new
     promo.get_promo_titles.keys.length
   end
+
+  def to_price(integer)
+    integer.to_s.scan(/./).insert(-3,'.').unshift('$').join
+  end
 end
