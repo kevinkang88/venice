9f345167f4b53d4eaaa09cb09e26144d428bb68e
Kevin Kang <kevinkang88@gmail.com>
2014-08-20 21:58:54 -0700

    ajaxified detailed page to show up on the right side. also index promo page is bootstraped with 2 columns that take up half of the page

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -3,12 +3,27 @@ class PromotionsController < ApplicationController
   def index
     promotions = Promotion.new
     @all_promo_titles = promotions.get_promo_titles
+    if params['promoId']
+      promo_id = params['promoId']
+      @promo_details=Promotion.get_promo_details(promo_id)
+      respond_to do |format|
+        format.html
+        format.json {render :json=> @promo_details}
+      end
+    end
   end
+
+
+
 
   def show
     promo_id = params['id']
     # require 'pry'; binding.pry
     @promo_details = Promotion.get_promo_details(promo_id)
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
 end
